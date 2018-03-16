3f8a6d1ca181a9d91b9a4d2fbc58097fd4cd73b9
Pranav Naik <naikpranav00@gmail.com>
2018-03-16 12:13:48 +0530

    Initial commit
    Pranav Text app

---
+++ Vidly/Migrations/201803160557534_AddDumyDataToMemberShipType.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDumyDataToMemberShipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Insert into MembershipTypes (Id, SingUpFee, DurationInMonths, DiscountRate, Name) VALUES (1, 0, 0, 0, A)");
+            Sql("Insert into MembershipTypes (Id, SingUpFee, DurationInMonths, DiscountRate, Name) VALUES (2, 30, 1, 10, A)");
+            Sql("Insert into MembershipTypes (Id, SingUpFee, DurationInMonths, DiscountRate, Name) VALUES (3, 90, 3, 15, A)");
+            Sql("Insert into MembershipTypes (Id, SingUpFee, DurationInMonths, DiscountRate, Name) VALUES (4, 300, 12, 20, A)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
