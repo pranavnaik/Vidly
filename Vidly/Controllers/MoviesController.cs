1a176c46a487e513e76716df3ae29c795392ba1b
Pranav Naik <naikpranav00@gmail.com>
2018-03-16 11:04:55 +0530

    Initial commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Jobs" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer1" },
+                new Customer {Name = "Cust 2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+            //return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+
+    }
+}
